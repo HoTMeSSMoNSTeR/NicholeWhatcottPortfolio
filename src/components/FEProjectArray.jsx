e0271b777df248424b321389b7299a037333dd67
Nichole <hotmessmonster87@gmail.com>
2023-08-30 03:08:49 -0600

    Create project array
    creating the array that will hold project information

---
+++ src/components/FEProjectArray.jsx
@@ -0,0 +1,11 @@
+export default [
+  {
+    id:"1", name:"Random Horror Movie Picker", previewImg:"", projectURL:"https://github.com/HoTMeSSMoNSTeR/Random-HorrorMovie-Picker", videoURL:""
+  } , 
+  {
+    id:"2", name:"Breast Cancer Awareness", previewImg:"", projectURL:"https://github.com/HoTMeSSMoNSTeR/BreastCancerAwarenessMonth", videoURL:""
+  } , 
+  {
+    id:"3", name:"Gaming Collection Site", previewImg:"", projectURL:"https://github.com/HoTMeSSMoNSTeR/Gaming-Final-Project", videoURL:""
+  }  
+]
