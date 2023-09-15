e73c5ee6a0d89365198e55a843036f801f28852f
Nichole <hotmessmonster87@gmail.com>
2023-09-14 18:50:13 -0600

    updates
    updates to possible contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,14 +1,34 @@
-import React from 'react'
+import React, {useRef} from 'react'
+// import emailjs from 'emailjs'
 import 'bootstrap/dist/css/bootstrap.css';
 
 
 export default function Contact() {
 
+    const form = useRef();
+
+    function sendEmail(e) {
+        e.preventDefault();
+        
+        emailjs.sendForm('service_wxle4fi', 'template_s8oftpj', form.current, 'kY2gwtA_SiZklUywK')
+        .then((result) => {
+            console.log(result.text)
+        }, (error) => {
+            console.log(error.text);
+        });
+
+    };
+
+
     return(
         <div>
-            <form action='mailto:hotmessmonster@gmail.com' method="post" className="contact-form">
+            <form ref={form} className="contact-form" id="contact-form" onSubmit={sendEmail}>
                 <label className="form-label email-form" htmlFor="name">
                     Name: <input type="text" id="name" className="form-control-lg" />
+                </label>
+                <br />
+                <label className="form-label email-form" htmlFor="affil">
+                    Company/Affiliation: <input type="text" id="affil" className="form-control-lg" />
                 </label>
                 <br />
                 <label className="form-label email-form" htmlFor="email">
@@ -16,8 +36,9 @@ export default function Contact() {
                 </label>
                 <br />
                 <label className="form-label email-form" htmlFor="message">
-                    Message: <textarea className="form-control-lg" rows={5}/>
+                    Message: <textarea className="form-control-lg" rows={2}/>
                 </label>
+                <input type="submit" value="Send" />
             </form>
         </div>
     )
